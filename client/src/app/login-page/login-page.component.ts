d2ef17950f604ebded75dfc44c58e461a1a77db2
vadym <vadymvladymyrovich@gmail.com>
2023-05-02 16:06:12 +0300

    third commit

--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../shared/services/auth.service";
+import {Subscription} from "rxjs";
+import {ActivatedRoute, Params, Router} from "@angular/router";
+
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss']
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit, OnDestroy{
+  form: FormGroup | any
+  aSub?: Subscription
 
+  constructor(private auth: AuthService,
+              private router: Router,
+              private route: ActivatedRoute) {
+  }
+  ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl (null, [
+        Validators.required,
+        Validators.email
+      ]),
+      password: new FormControl (null,[
+        Validators.required,
+        Validators.minLength(6)
+      ])
+    })
+      this.route.queryParams.subscribe((params: Params) => {
+        if (params['registered']) {
+        //   Тепер ви можете увійти до системи використовуючи свої данні.
+        } else if (params['accessDenied']) {
+        // Спочатку авторизуйтеся.
+        }
+      })
+  }
+  ngOnDestroy() {
+    if (this.aSub){
+      this.aSub.unsubscribe()
+    }
+  }
+
+  onSubmit() {
+    this.form.disable()
+
+    this.aSub = this.auth.login(this.form.value).subscribe(
+     () => this.router.navigate(['/overview']),
+     error => {
+       console.warn(error)
+       this.form.enable()
+     }
+   )
+  }
 }
