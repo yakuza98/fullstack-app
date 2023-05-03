a1b2461844d703e08fb79032be0dada871e42582
vadym <vadymvladymyrovich@gmail.com>
2023-05-03 17:09:59 +0300

    others commit about categories form

---
+++ client/src/app/categories-page/categories-form/positions-form/positions-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-positions-form',
+  templateUrl: './positions-form.component.html',
+  styleUrls: ['./positions-form.component.scss']
+})
+export class PositionsFormComponent {
+
+}
